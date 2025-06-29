3cf07717ff40c84219d78ae310a894e50828fd9b
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-29 14:51:54 -0600

    add LoadMoreBtnProps

--- src/components/App/App.types.ts
+++ src/components/App/App.types.ts
@@ -34,3 +34,7 @@ export type ImageModalProps = {
   src?: string;
   alt?: string;
 };
+
+export type LoadMoreBtnProps = {
+  onClick: () => void;
+};
--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,6 +1,7 @@
+import type { LoadMoreBtnProps } from "../App/App.types";
 import s from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ onClick }) => {
+const LoadMoreBtn = ({ onClick }: LoadMoreBtnProps) => {
   return (
     <button onClick={onClick} className={s.load_more}>
       Load more
